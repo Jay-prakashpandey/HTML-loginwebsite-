36f94255ad67d6f9e83fee5308ae5fe50742dbc0
Jay-prakashpandey <81908427+Jay-prakashpandey@users.noreply.github.com>
2021-10-25 23:58:54 +0530

    Add files via upload

---
+++ loginpage/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWTjiPDH9A_EnjmAAVsLTianazykm_Rv4",
+    authDomain: "loginwebsite-a7583.firebaseapp.com",
+    projectId: "loginwebsite-a7583",
+    storageBucket: "loginwebsite-a7583.appspot.com",
+    messagingSenderId: "1073172818430",
+    appId: "1:1073172818430:web:a7318fed0a2c42f418f8af"
+  };
+
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
+  //firebaseConfig.analytics();
